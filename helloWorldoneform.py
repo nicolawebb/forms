f6296e55fa7a9e5237ff3737b198ac95506dd3ae
Nicola Webb <Nicola6.Webb@uwe.ac.uk>
2022-03-07 14:29:34 +0000

    New files!

---
+++ helloWorldoneform.py
@@ -0,0 +1,22 @@
+#! /Python35/python
+
+import cgi
+import cgitb      #use for error traces
+cgitb.enable()    #use for error traces
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields  
+name = form.getvalue('name')        # name of the field is same as in client form
+module  = form.getvalue('module')   # name of the field is same as in client form
+
+print ("Content-type:text/html\n")
+print ("<HTML>")
+print ("<HEAD>")
+print ("<TITLE>Hello - Form processing</TITLE>")
+print ("</HEAD>")
+print ("<BODY>")
+print ("<H1>Hello ", name, " You're enrolled to study ", module, ". Enjoy!</H1>")
+print ("</BODY>")
+print ("</HTML>")
